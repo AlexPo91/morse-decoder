35e561739cc4ee7ee363f9ab77949f9a57393461
Alex Polyachinscky <polya4inscky.alex@yandex.ru>
2020-03-16 22:49:37 +0300

    feat: add solution task

--- src/index.js
+++ src/index.js
@@ -39,6 +39,13 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    let str
+    let strDecode =''
+    for (let i = 0; i < expr.length; i+=10){
+        str = (expr.slice(i, i+10)).replace(/10/g, '.').replace(/11/g, '-').replace(/00/g, '')
+        strDecode += MORSE_TABLE[str] || ' '
+    }
+   return strDecode
 }
 
 module.exports = {
